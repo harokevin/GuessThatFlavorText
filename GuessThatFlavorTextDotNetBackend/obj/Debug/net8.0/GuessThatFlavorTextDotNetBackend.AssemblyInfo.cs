6dc7b32be17ec8076f72621435ab44ef9ad49bba
Kevin Haro <harokevin@Mac.lan>
2026-06-29 14:26:48 -0700

    Revert 7e9fc6535e05f06c60b690086bb3d95bf48c432f

--- GuessThatFlavorTextDotNetBackend/obj/Debug/net8.0/GuessThatFlavorTextDotNetBackend.AssemblyInfo.cs
+++ GuessThatFlavorTextDotNetBackend/obj/Debug/net8.0/GuessThatFlavorTextDotNetBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuessThatFlavorTextDotNetBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f43a3c27c0345adbc47dd1b13eb5edc4640b3046")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+067e2dd268880814ab9d911dd2715a737d71562b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuessThatFlavorTextDotNetBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuessThatFlavorTextDotNetBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
